3b581ad9346aa3d25105228c0faf12488f2f8344
atkonline <atkonline@gmail.com>
2019-05-23 16:21:47 +0100

    submit

--- freshbutkeen-app/src/app/app-routing.module.ts
+++ freshbutkeen-app/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { SubmitTicketComponent }         from './submit-ticket/submit-ticket.component';
 import { DashboardComponent }   from './dashboard/dashboard.component';
 import { HeroesComponent }      from './heroes/heroes.component';
 import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
@@ -9,7 +9,8 @@ const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: HeroDetailComponent },
-  { path: 'heroes', component: HeroesComponent }
+  { path: 'heroes', component: HeroesComponent },
+  { path: 'submitTicket', component: SubmitTicketComponent }
 ];
 
 @NgModule({
